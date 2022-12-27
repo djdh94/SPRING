8bb939d74e18eebae8d28dc607c76775df6c428b
djdh94 <djdh94@naver.com>
2022-12-27 20:54:20 +0900

    controller-@mapping,@pathvariable,@requestparam

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/b9/909c7ec2d185001d11e68b079f672ec2
@@ -0,0 +1,10 @@
+package com.ict.controller.di.classfile;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Singer {
+	public void sing() {
+		System.out.println("가수가 노래를 합니다.");
+	}
+}
---
+++ SpringBasic/src/main/java/com/ict/controller/vo/UserVO.java
@@ -0,0 +1,12 @@
+package com.ict.controller.vo;
+
+import lombok.Data;
+
+@Data
+public class UserVO {
+	private int userNum;
+	private String userId;
+	private String userPw;
+	private String userName;
+	private int userAge;
+}
