521d86c639860d9c56e06a2922fea7af50f98df9
djdh94 <djdh94@naver.com>
2023-01-03 20:39:44 +0900

    mybatis,VO

--- SpringBoard/src/main/java/com/ict/mapper/TimeMapper.java
+++
@@ -1,9 +0,0 @@
-package com.ict.mapper;
-
-import org.apache.ibatis.annotations.Select;
-
-public interface TimeMapper {
-
-	@Select("select SYSDATE FROM dual")
-	public String getTime();
-}
--- SpringBoard/src/test/java/com/ict/persistance/TimeMapperTests.java
+++ SpringBoard/src/test/java/com/ict/persistance/TimeMapperTests.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import com.ict.mapper.TimeMapper;
+import com.ict.time.mapper.TimeMapper;
 
 import lombok.extern.log4j.Log4j;
 
@@ -18,9 +18,14 @@ public class TimeMapperTests {
 	@Autowired
 	private TimeMapper timeMapper;
 	
-	@Test
+	//@Test
 	public void testGetTime() {
 		log.info("현재시간 조회");
 		log.info(timeMapper.getTime());
+	}
+	
+	@Test
+	public void testGetTime2() {
+		log.info(timeMapper.getTime2());
 	}
 }
